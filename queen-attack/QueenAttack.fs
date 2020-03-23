a4146c7f1ad7d3aa358c337c382fe40a5e1c4ebb
Mark Dobbins <mjdobbins12@gmail.com>
2020-03-23 23:54:36 +0000

    what is this chess you speak of

---
+++ queen-attack/QueenAttack.fs
@@ -0,0 +1,7 @@
+﻿module QueenAttack
+
+let create ((a, b): int * int) =
+    a >= 0 && a < 8 && b >= 0 && b < 8
+
+let canAttack ((q1_row, q1_column): int * int) ((q2_row, q2_column): int * int) =
+    q1_row = q2_row || q1_column = q2_column || abs (q1_row - q2_row) = abs (q1_column - q2_column)
