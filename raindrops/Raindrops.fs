7bf831929be5b5ea5bf1b50bd6814489ba89b6ec
Mark Dobbins <mjdobbins12@gmail.com>
2020-03-26 23:35:45 +0000

    classic one before bed

--- raindrops/Raindrops.fs
+++ raindrops/Raindrops.fs
@@ -2,5 +2,11 @@
 
 let convert (number: int): string =
     match number with
-    | a when a % 3 = 0 -> "Pling"
+    | a when a % 105 = 0 -> "PlingPlangPlong"
+    | b when b % 15 = 0 -> "PlingPlang"
+    | c when c % 21 = 0 -> "PlingPlong"
+    | d when d % 35 = 0 -> "PlangPlong"
+    | e when e % 3 = 0 -> "Pling"
+    | f when f % 5 = 0 -> "Plang"
+    | g when g % 7 = 0 -> "Plong"
     | _ -> string number
