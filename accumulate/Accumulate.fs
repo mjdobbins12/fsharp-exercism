a7e341189f98225417d9ff7285bd9fd4ac0a93d4
Mark Dobbins <mjdobbins12@gmail.com>
2020-03-27 17:08:37 +0000

    manual map

---
+++ accumulate/Accumulate.fs
@@ -0,0 +1,8 @@
+﻿module Accumulate
+
+let accumulate (func: 'a -> 'b) (input: 'a list): 'b list =
+    let rec loop inp acc =
+        match inp with
+        | head :: tail -> loop tail (func head :: acc)
+        | [] -> List.rev acc
+    loop input List.empty
