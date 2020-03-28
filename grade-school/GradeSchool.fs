852ac352612771e987858ab9311ce15f0854141c
Mark Dobbins <mjdobbins12@gmail.com>
2020-03-28 17:58:08 +0000

    getting somewhere

---
+++ grade-school/GradeSchool.fs
@@ -0,0 +1,15 @@
+﻿module GradeSchool
+
+type School = Map<int, string list>
+
+let empty: School = Map.empty
+
+let add (student: string) (grade: int) (school: School): School =
+    school.Add(grade, [student])
+
+let roster (school: School): string list =
+    school
+    |> Map.toList
+    |> List.collect (fun (_, students) -> List.sort students)
+
+let grade (number: int) (school: School): string list = failwith "You need to implement this function."
