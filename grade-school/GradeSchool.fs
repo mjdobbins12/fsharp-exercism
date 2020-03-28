6a7dd3a6cccc809608c46ae2331fa19e37c40608
Mark Dobbins <mjdobbins12@gmail.com>
2020-03-28 18:03:47 +0000

    making more sense now

--- grade-school/GradeSchool.fs
+++ grade-school/GradeSchool.fs
@@ -5,7 +5,9 @@ type School = Map<int, string list>
 let empty: School = Map.empty
 
 let add (student: string) (grade: int) (school: School): School =
-    school.Add(grade, [student])
+    match Map.tryFind grade school with
+    | Some x -> Map.add grade (student :: x) school
+    | None -> Map.add grade [student] school
 
 let roster (school: School): string list =
     school
