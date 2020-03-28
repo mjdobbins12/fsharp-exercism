70f19af8ffd066ac8b054b28ad8da810fcf77000
Mark Dobbins <mjdobbins12@gmail.com>
2020-03-28 00:18:24 +0000

    ok numerics are kinda weird here

---
+++ space-age/SpaceAge.fs
@@ -0,0 +1,18 @@
+﻿module SpaceAge
+
+type Planet(name: string, orbitalPeriod: float) =
+    member this.name = name
+
+    member this.orbitalPeriod = orbitalPeriod * 31557600.0
+
+let Mercury = Planet("Mercury", 0.2408467)
+let Venus = Planet("Venus", 0.61519726)
+let Earth = Planet("Earth", 1.0)
+let Mars = Planet("Mars", 1.8808158)
+let Jupiter = Planet("Jupiter", 11.862615)
+let Saturn = Planet("Saturn", 29.447498)
+let Uranus = Planet("Uranus", 84.016846)
+let Neptune = Planet("Neptune", 164.79132)
+
+let age (planet: Planet) (seconds: int64): float =
+    float(seconds) / planet.orbitalPeriod
