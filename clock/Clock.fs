2025755992e023e52338471ecc5d05e0f36bd0c6
Mark Dobbins <mjdobbins12@gmail.com>
2020-04-02 23:11:46 +0100

    negative numbers are lame

--- clock/Clock.fs
+++ clock/Clock.fs
@@ -3,16 +3,17 @@ module Clock
 let create hours minutes =
     let adjustMins minutes =
         match minutes with
-        | a when a < 0 -> (minutes / 60 - 1, abs (minutes + 60) % 60)
+        | a when a < -60 -> (minutes / 60 - 1, 60 - abs minutes % 60)
+        | b when b < 0 -> (-1, minutes + 60)
         | _ -> (minutes / 60, minutes % 60)
 
     let rec adjustedHrs hours =
         match hours with
         | a when a < 0 -> adjustedHrs (hours + 24)
-        | _ -> hours 
+        | _ -> hours % 24
     
     adjustMins minutes
-    |> (fun (a, b) -> (((a + adjustedHrs hours) % 24), b))
+    |> (fun (a, b) -> adjustedHrs (hours + a), b)
 
 let add minutes clock = failwith "You need to implement this function."
 
