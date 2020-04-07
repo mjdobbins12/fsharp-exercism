4f3ba4f17212569bc6edd01704a77f4b1b15fda8
Mark Dobbins <mjdobbins12@gmail.com>
2020-04-07 16:29:35 +0100

    beer

---
+++ beer-song/BeerSong.fs
@@ -0,0 +1,7 @@
+﻿module BeerSong
+
+let recite (startBottles: int) (takeDown: int) =
+    [
+        string startBottles + " bottles of beer on the wall, " + string startBottles + " bottles of beer.";
+        "Take one down and pass it around, " + string (startBottles - 1) + " bottles of beer on the wall."
+    ]
