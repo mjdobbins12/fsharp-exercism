1d0fb98215a8b74a5572e4fcdccbda49579be7c1
Mark Dobbins <mjdobbins12@gmail.com>
2020-04-10 17:04:05 +0100

    need to dig into collections a bit more

---
+++ kindergarten-garden/KindergartenGarden.fs
@@ -0,0 +1,17 @@
+﻿module KindergartenGarden
+
+type Plant = Clover | Grass | Radishes | Violets
+
+let kids = ["Alice"; "Bob"; "Charlie"; "David"; "Eve"; "Fred"; "Ginny"; "Harriet"; "Ileana"; "Joseph"; "Kincaid"; "Larry"]
+
+let plants (diagram: string) (student: string) =
+    let ind = List.findIndex (fun x -> x = student) kids
+   
+    diagram.Split '\n'
+    |> Seq.collect (Seq.toList >> (List.chunkBySize 2) >> (fun x -> x.[ind]))
+    |> Seq.map (function
+        | 'C' -> Plant.Clover
+        | 'G' -> Plant.Grass
+        | 'R' -> Plant.Radishes
+        | 'V' -> Plant.Violets)
+    |> Seq.toList
