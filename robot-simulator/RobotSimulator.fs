1a659ffe87fc6b8d23d09524ceb1347aca72fe21
Mark Dobbins <mjdobbins12@gmail.com>
2020-04-13 11:33:12 +0100

    records

---
+++ robot-simulator/RobotSimulator.fs
@@ -0,0 +1,10 @@
+﻿module RobotSimulator
+
+type Direction = North | East | South | West
+type Position = int * int
+type Robot = { direction: Direction; position: Position }
+
+let create direction position =
+    {direction = direction; position = position} 
+
+let move instructions robot = failwith "You need to implement this function."
