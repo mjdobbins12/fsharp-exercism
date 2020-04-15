9f0fa873a368def9a573efdc10f68a64870a001b
Mark Dobbins <mjdobbins12@gmail.com>
2020-04-15 20:39:00 +0100

    bitwise operators are dope!

---
+++ allergies/Allergies.fs
@@ -0,0 +1,19 @@
+﻿module Allergies
+
+open System
+
+[<FlagsAttribute>]
+type Allergen =
+    | Eggs = 1
+    | Peanuts = 2
+    | Shellfish = 4
+    | Strawberries = 8
+    | Tomatoes = 16
+    | Chocolate = 32
+    | Pollen = 64
+    | Cats = 128
+
+let allergicTo (codedAllergies: int) allergen =
+    (enum<Allergen>(codedAllergies) &&& allergen) = allergen
+
+let list codedAllergies = failwith "You need to implement this function."
